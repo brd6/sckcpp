4de0d5219c7a4675b3bec6ca4dc869f47e98540a
Berdrigue BONGOLO-ETO <b.berdrigue@gmail.com>
2017-09-06 01:43:48 +0200

    base socket in progress

--- include/sckcpp/BaseSocket.hpp
+++ include/sckcpp/BaseSocket.hpp
@@ -2,17 +2,45 @@
 // Created by brd6 on 06/09/17.
 //
 
+#include <bits/socket.h>
+
 #ifndef BaseSocket_H_
 # define _BaseSocket_H_
 
 namespace sckcpp
 {
+  enum SocketType
+  {
+    TCP,
+    UDP
+  };
+
+  enum SocketDomain
+  {
+    IP = AF_INET,
+    IPV6 = AF_INET6,
+    LOCAL_UNIX = AF_UNIX,
+    LOCAL = AF_LOCAL
+  };
+
+  enum Network
+  {
+    
+  };
+
   class BaseSocket
   {
    private:
+    int mSockFd;
+
+   protected:
+    BaseSocket() = default;
 
    public:
-    
+    virtual ~BaseSocket() = default;
+    BaseSocket(BaseSocket const &) = delete;
+    BaseSocket &operator=(BaseSocket const &) = delete;
+
   };
 }
 
---
+++ include/sckcpp/SocketException.hpp
@@ -0,0 +1,28 @@
+//
+// Created by brd6 on 06/09/17.
+//
+
+
+#ifndef _SocketException_H_
+# define _SocketException_H_
+
+#include <string>
+#include <exception>
+
+namespace sckcpp
+{
+  class SocketException :
+	  public std::exception
+  {
+   private:
+    std::string mMsg;
+
+   public:
+    explicit SocketException(std::string const &msg) : mMsg(msg) {}
+
+    const char* what() const throw() override { return mMsg.data(); }
+
+  };
+}
+
+#endif /* !_SocketException_H_ */
